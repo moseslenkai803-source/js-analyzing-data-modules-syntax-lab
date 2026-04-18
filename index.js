24f1dae2921261609e54c702d5bc55d16f83d374
Moses lenkai <moseslenkai803@gmail.com>
2026-04-18 15:11:37 +0300

    merdegedall arrays into one, created a date of merging

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
 
 
+
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+function combineUsers(...arrays) {
+  // Merge all arrays into one
+  const users = [].concat(...arrays);
+  
+  // Create a date of merging, formatted as M/d/yyyy
+  const mergeDate = new Date();
+  const merge_date = mergeDate.toLocaleDateString('en-US', { 
+    month: 'numeric', 
+    day: 'numeric', 
+    year: 'numeric' 
+  });
+  
+  // Return an object with merged array and date
+  return {
+    users: users,
+    merge_date: merge_date
+  };
+}
